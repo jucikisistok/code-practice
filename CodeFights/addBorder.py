4ab56de4d956e3459e210e5896d4bbc4ea503d31
Juci Kisistók <judit.kisistok@gmail.com>
2017-07-26 15:18:23 +0200

    new solution added

---
+++ CodeFights/addBorder.py
@@ -0,0 +1,13 @@
+def addBorder(picture):
+	"""
+	Given a rectangular matrix of characters, this 
+	adds a border of asterisks(*) to it.
+	"""
+	
+	for i in range(len(picture)):
+		picture[i] = "*" + picture[i] + "*"
+
+	picture.insert(0, "*"*len(picture[0]))
+	picture.append("*"*len(picture[0]))
+	return picture
+
